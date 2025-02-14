d1259f350cc2c7cf03db469785f1d2cb934defff
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-02-14 18:44:44 +0700

    test: add take screenshot

--- tests/budget-calculator.spec.ts
+++ tests/budget-calculator.spec.ts
@@ -8,14 +8,20 @@ test("should able to go to budget calculator page, fill in form and get expense 
   // Finanical calculator
   await financialCalculatorPage.goto();
   await financialCalculatorPage.cookieNoticeBanner.clickAgreeCookieButton();
+  await expect(page).toHaveScreenshot('financial-calculator-page.png');
+
   await financialCalculatorPage.clickBudgetCalculatorPage();
+  await expect(page).toHaveScreenshot('budget-calculator-page.png');
 
   // Budget calculator page
   const budgetCalculatorPage = new BudgetCalculatorPage(page);
 
   await budgetCalculatorPage.typeTakeHomeIncomeInput("20000");
   await budgetCalculatorPage.typeZipCodeInput("94302");
+  await expect(page).toHaveScreenshot('budget-calculator-page-form.png');
+
   await budgetCalculatorPage.submitForm();
 
   await expect(budgetCalculatorPage.expenseBudgetContent).toBeVisible();
+  await expect(page).toHaveScreenshot('budget-calculator-page-content.png');
 });
