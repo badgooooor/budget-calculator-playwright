455ac20397b25c0d37a5c23238459dcac8c79cf3
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-02-14 17:51:00 +0700

    Add input model

---
+++ app/components/formInput.ts
@@ -0,0 +1,20 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export interface IFormInput {
+  type(text: string): Promise<void>;
+};
+
+export class FormInput implements IFormInput {
+  readonly input: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(page: Page, name: string) {
+    this.input = page.locator(`#floating_label_input_${name}`);
+    this.errorMessage = page.getByTestId(`${name}-error`);
+  }
+
+  async type(text: string) {
+    await expect(this.input).not.toBeDisabled();
+    this.input.fill(text);
+  };
+}
